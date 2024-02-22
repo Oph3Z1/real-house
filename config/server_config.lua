522e8b1530702aa13f8e7f9d3449a7c1280b3ff3
Oph3Z <59979752+Oph3Z1@users.noreply.github.com>
2024-02-22 12:56:33 +0100

    2.0.4

--- config/server_config.lua
+++ config/server_config.lua
@@ -1,4 +1,4 @@
-Config.DiscordBotToken = 'OTMwODI3Mzg1MzI5MzA3NzMx.GQPatL.q0qjstbgFANq6d21rMjZK7A4v__UmNNxF0dti8' -- Discord bot token
+Config.DiscordBotToken = '' -- Discord bot token
 Config.PhoneMail = "qb-phone:server:sendNewMailToOffline" -- Send mail event
 
 RegisterNetEvent('real-house:SendMailToPlayerHouseExpired', function(player)
