d50a9c0e256f95cf0c783b698e1801e5198c7cf5
Oph3Z <59979752+Oph3Z1@users.noreply.github.com>
2024-02-22 14:51:59 +0100

    2.0.4

--- real-house.sql
+++ real-house.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS `real_house` (
   `keydata` varchar(50) DEFAULT NULL,
   `rentowner` longtext DEFAULT NULL,
   `allowrent` varchar(50) DEFAULT NULL,
-  `friends` longtext DEFAULT NULL
+  `friends` longtext DEFAULT NULL,
+  `payment` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
